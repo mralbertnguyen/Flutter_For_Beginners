36b63b48d5db55037e9de9b4a7295b2fe2659c34
AlbertNguyen <ndttam.dev@gmail.com>
2019-03-05 11:58:24 +0700

    Login - Added check account is correct with default account and change to target screen

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'main.dart';
 void main() => runApp(Login());
 
 class Login extends StatelessWidget{
@@ -23,28 +25,118 @@ class LoginPage extends StatefulWidget{
 }
 
 class LoginPageState extends State<LoginPage>{
+
+  // Create controller
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // Account default
+  final String userNameDefault = 'admin';
+  final String passwordDefault = 'admin';
+
+  // Function check login
+  void _checkInfoLogin(){
+
+    final String _username = userNameController.text;
+    final String _password = passwordController.text;
+
+    if(checkNull(_username, _password)){
+      print("Null");
+    }
+
+    if(!checkIsCorrect(_username, _password)){
+      print("Not Correct");
+    }else{
+      loginSucess();
+    }
+
+  }
+
+  bool checkNull (String username, String password){
+    if(username.length == 0 && password.length == 0){
+      return true;
+    }
+    return false;
+  }
+
+  bool checkIsCorrect(String username, String password){
+    if(username.compareTo(userNameDefault) != 0 && password.compareTo(passwordDefault) != 0){
+      return false;
+    }
+    return true;
+  }
+
+
+  void loginSucess(){
+    // change to main screen
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MyApp()),
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    // Clean up controller when Widget is disposed
+    userNameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       body: new Column(
         children: <Widget>[
-          inputTextField("User Name: "),
-          inputTextField("Password: ")
+          inputTextField("User Name: ", userNameController),
+          inputTextField("Password: ", passwordController),
+          buttonFrom("Login"),
         ],
-        
-      );
+      )
     );
   }
 
-  static TextField inputTextField(String placholder){
+  TextField inputTextField(String placholder, TextEditingController controller){
     return TextField(
+      controller: controller,
       decoration: InputDecoration(
         labelText: placholder
       ),
     );
   }
 
+ RaisedButton buttonFrom (String btnTitle){
+    return RaisedButton(
+      onPressed: (){
+        _checkInfoLogin();
+      },
+      child: new Text(btnTitle),
+    );
+  }
+
+
+  AlertDialog alertNotification(String title, String content){
+    return AlertDialog(
+      title: new Text(title),
+      content: new Text(content),
+    );
+  }
 
 }
 
+
+//class Main extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return MaterialApp(
+//        home : new Scaffold(
+//          body: new Center(
+//            child: new Text("Home page"),
+//          ),
+//        )
+//    );
+//  }
+//}
